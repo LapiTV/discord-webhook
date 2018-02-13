dd5aa8cb35777708bd3fcb8a5690099400490aa7
WFrancois <wroblewski.francois@gmail.com>
2018-02-13 13:35:53 +0100

    Add deployer

---
+++ deploy.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Project name
+set('application', 'discord-webhook-lapi');
+
+// Project repository
+set('repository', 'git@github.com:WFrancois/discord-webhook-lapi.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+set('shared_files', ['.env', 'db.json']);
+
+// Hosts
+host('ns378030')
+    ->set('deploy_path', '/home/projects/{{application}}');
+
+// Tasks
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:clear_paths',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+])->desc('Deploy your project');
